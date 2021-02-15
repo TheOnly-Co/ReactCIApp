ca407c3cea17d2a7de54f6afa87d43dc5c697384
mit <mit945.mc@gmail.com>
2021-02-14 17:56:15 -0800

    hi

---
+++ test/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'; 
+import './App.css';
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      name: "Test App",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPTJuvquCwL7eCHn45YEYven_6f4sTStbXMw&usqp=CAU"
+    }
+  }
+  render (){
+    return (
+      <>
+      <div className="App">
+        <h1>Welcome, This is {this.state.name} !</h1>
+        <img src= {this.state.img} alt="" width="500" height="600"/>
+      </div>
+      </>
+    );
+  }
+
+
+
+}
+
+export default App;
