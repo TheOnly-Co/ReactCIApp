a29df2dd1f9a48a07a523f209035f1ae616180a2
mit <mit945.mc@gmail.com>
2021-03-27 16:27:52 -0700

    add in app.js and push diff docker image

--- test/src/App.js
+++ test/src/App.js
@@ -5,6 +5,7 @@ class App extends Component{
     super(props)
     this.state = {
       name: "store App",
+      anime: "Full metal Alchemist",
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPTJuvquCwL7eCHn45YEYven_6f4sTStbXMw&usqp=CAU"
     }
   }
@@ -13,7 +14,7 @@ class App extends Component{
       <>
       <div className="App">
         <h1>Welcome, This is {this.state.name} !</h1>
-        <p> I am tired </p>
+        <p> Below anime is {this.state.anime}! </p>
         <img src= {this.state.img} alt="" width="500" height="600"/>
       </div>
       </>
